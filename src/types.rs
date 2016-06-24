e49621f983a8ddba31483093c630903165cdcf8e
Cristian Medeiros <cristianmedeiros@gmail.com>
2016-06-23 22:39:44 -0300

    Fixing is_single_arg, it wasn't checking for multiple arguments

--- src/types.rs
+++ src/types.rs
@@ -423,7 +423,7 @@ pub trait ToRedisArgs: Sized {
     /// is used in some high level functions to intelligently switch
     /// between `GET` and `MGET` variants.
     fn is_single_arg(&self) -> bool {
-        true
+        &self.len() == 1 
     }
 
     /// This only exists internally as a workaround for the lack of
